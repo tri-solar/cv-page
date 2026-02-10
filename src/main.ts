0bdc1a4a2b0cc4eafa463e62d8ddebf9c1ab5185
tri-solar <florianklocke@protonmail.com>
2026-02-10 22:45:29 +0100

    cleanup and initial scene

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,70 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/**
+ * Canvas
+ */
+const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Geometry & Material
+ */
+const geometry = new THREE.SphereGeometry(1, 32, 32)
+const material = new THREE.MeshStandardMaterial({ color: '#42DAED' })
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.35)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const tick = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
