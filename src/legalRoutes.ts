91509d952a79d4f0bfb6c5295671f4be05ad31ae
tri-solar <florianklocke@protonmail.com>
2026-09-07 19:06:10 +0200

    fix imprint and privacy policy page behaviour

--- src/legalRoutes.ts
+++ src/legalRoutes.ts
@@ -2,6 +2,10 @@ const legalRoutes = new Set(['/imprint', '/privacy-policy']);
 
 export const LEGAL_CAMERA_TURN_ANGLE = Math.PI * 0.25;
 
+function normalizePath(pathname: string): string {
+  return pathname.replace(/\/+$/, '') || '/';
+}
+
 export function isLegalRoute(pathname: string): boolean {
-  return legalRoutes.has(pathname);
+  return legalRoutes.has(normalizePath(pathname));
 }
